c3c0630011be9636ad085aaac1feb2d80bf2a336
dexyurisuba-droid <dexyurisuba@gmail.com>
2026-09-20 15:15:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ for (let i = 0; i < stars.length; i++) {
   
   stars[i].addEventListener("click", function() {
     
-    selectedRating = this.getAttribute("data-v");
+    selectedRating = Number(this.getAttribute("data-v"));
     
     for (let j = 0; j < stars.length; j++) {
-      if (stars[j].getAttribute("data-v") <= selectedRating) {
+      if (Number(stars[j].getAttribute("data-v")) <= selectedRating) {
         stars[j].style.color = "#ffb400"; 
       } else {
         stars[j].style.color = "#ccc"; 
@@ -35,6 +35,31 @@ toggleFormBtn.addEventListener("click", function() {
     addForm.style.display = "none";
     toggleFormBtn.innerText = "+ Write a review";
   }
+});
+
+reviewForm.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  
+  if (selectedRating == 0) {
+    alert("Please choose a star rating first.");
+    return;
+  }
+
+  let name = document.getElementById("nameInput").value;
+  
+  thankYouMsg.innerText = "Salamat, " + name + "! Thank you for your " + selectedRating + "-star review for Joey's Restaurant Café.";
+  
+  alert("Thank you for your feedback, " + name + "!");
+  
+  reviewForm.reset();
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].style.color = "#ccc";
+  }
+  selectedRating = 0;
+  
+  addForm.style.display = "none";
+  toggleFormBtn.innerText = "+ Write a review";
+});  }
 });
 
 reviewForm.addEventListener("submit", function(event) {
